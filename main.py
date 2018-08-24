f08cba83f42aac65c2875273ebe041a233ee6a0a
linchen <linchen@firedata.cc>
2018-08-23 22:57:39 -0400

    lstm_model

--- main.py
+++ main.py
@@ -13,115 +13,260 @@ from baseline import create_file_vector
 
 import numpy as np
 np.random.seed(123)  # for reproducibility
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
-from keras.utils import np_utils
+import json
+import tensorflow as tf
+import os
+from idcnn import Model as IdCNN
+from bilstm import Model as BiLSTM
+FLAGS = tf.app.flags.FLAGS
+from sklearn.metrics import accuracy_score
 
-class BaselineModel(object):
-    """This is used to do the baseline predict"""
-    def __init__(self, embedding_size):
-        self.embedding_size = embedding_size
-    
-    def fit(self, value_c, model_save_path, train_vector, train_label):
-        log.info("Start fitting the model")
-        classifier = svm.LinearSVC(random_state = 0, C = value_c)
-        classifier.fit(train_vector, train_label)
-        joblib.dump(classifier, model_save_path, compress=3)
-        log.info("End fitting the modle")
-        
-    def predict(self, model_save_path, file_vector_list):
-        log.info("Start predicting the model")
-        classifier = joblib.load(model_save_path)
-        predict_result = classifier.predict(file_vector_list)
-        return predict_result
-        
-def generate_model_input_data(train_word_file, train_label_file, dict_file, cv_ratio=0.8):
-    """This is used to generate the input data of model, also do the cv work"""
-    label = []
-    with open(train_label_file, "r") as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            line = line.replace("\n", "")
-            label.append(int(line))
-    
-    train_file_vector = create_file_vector(train_word_file, dict_file)
-    train_vector = []
-    train_label = []
-    train_cv_vector = []
-    train_cv_label = []
-    
-    for i in range(len(train_file_vector)):
-        if random.random() < cv_ratio:
-            train_vector.append(train_file_vector[i])
-            train_label.append(label[i])
+tf.app.flags.DEFINE_string('log_dir', "logs", 'The log  dir')
+tf.app.flags.DEFINE_string("word2vec_path", "/Users/flynn/Desktop/DaGuan/new_data/word_dic_64.json",
+                           "the word2vec data path")
+tf.app.flags.DEFINE_integer("max_sentence_len", 80,
+                            "max num of tokens per query")
+tf.app.flags.DEFINE_integer("embedding_size", 64, "embedding size")
+tf.app.flags.DEFINE_integer("num_tags", 14, "BMES")
+tf.app.flags.DEFINE_integer("num_hidden", 100, "hidden unit number")
+tf.app.flags.DEFINE_integer("batch_size", 100, "num example per mini batch")
+tf.app.flags.DEFINE_integer("train_steps", 1000, "trainning steps")
+tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate")
+tf.app.flags.DEFINE_bool("use_idcnn", False, "whether use the idcnn")
+tf.app.flags.DEFINE_integer("track_history", 6, "track max history accuracy")
+
+def do_load_data(path):
+    x = []
+    y = []
+    fp = open(path, "r")
+    for line in fp.readlines():
+        line = line.rstrip()
+        if not line:
+            continue
+        ss = line.split(" ")
+        assert (len(ss) == (FLAGS.max_sentence_len * 2))
+        lx = []
+        ly = []
+        for i in range(FLAGS.max_sentence_len):
+            lx.append(int(ss[i]))
+
+        ly.append(int(ss[1 + FLAGS.max_sentence_len]))
+        ly.append(int(ss[2 + FLAGS.max_sentence_len]))
+        ly.append(int(ss[3 + FLAGS.max_sentence_len]))
+        ly.append(int(ss[4 + FLAGS.max_sentence_len]))
+        x.append(lx)
+        y.append(ly)
+    fp.close()
+    return np.array(x), np.array(y)
+
+def inputs(path):
+    whole = read_csv(FLAGS.batch_size, path)
+    features = tf.transpose(tf.stack(whole[0:FLAGS.max_sentence_len]))
+    return features
+
+class Model:
+    def __init__(self, embeddingSize, distinctTagNum, c2vPath, numHidden):
+        self.embeddingSize = embeddingSize
+        self.distinctTagNum = distinctTagNum
+        self.numHidden = numHidden
+        self.c2v = self.load_w2v(c2vPath)
+        layers = [
+            {
+                'dilation': 1
+            },
+            {
+                'dilation': 1
+            },
+            {
+                'dilation': 2
+            },
+        ]
+        if FLAGS.use_idcnn:
+            self.model = IdCNN(layers, 3, FLAGS.num_hidden, FLAGS.embedding_size,
+                               FLAGS.max_sentence_len, FLAGS.num_tags)
         else:
-            train_cv_vector.append(train_file_vector[i])
-            train_cv_label.append(label[i])
-    return train_vector, train_label, train_cv_vector, train_cv_label
+            self.model = BiLSTM(
+                FLAGS.num_hidden, FLAGS.max_sentence_len, FLAGS.num_tags)
+        self.inp = tf.placeholder(tf.float32,
+                                  shape=[None, FLAGS.max_sentence_len, 64],
+                                  name="input_placeholder")
+        pass
+
+    def length(self, data):
+        if (tf.contrib.framework.is_tensor(data)):
+            data = tf.reshape(data[:,:,0], [-1, 80])
+        used = tf.sign(tf.abs(data[:, 0]))
+        length = tf.cast(used, tf.int32)
+        return length
+
+    def inference(self, X, reuse=None, trainMode=True):
+        if (tf.contrib.framework.is_tensor(X)):
+            word_vectors = X
+        else:
+            word_vectors = tf.convert_to_tensor(self.create_file_vector(X, embedding_size = 64), dtype=tf.float32)
+        length = self.length(X)
+        reuse = False if trainMode else True
+        if FLAGS.use_idcnn:
+            word_vectors = tf.expand_dims(word_vectors, 1)
+            unary_scores = self.model.inference(word_vectors, reuse=reuse)
+        else:
+            unary_scores = self.model.inference(
+                word_vectors, length, reuse=reuse)
+        return unary_scores, length
+
+    def loss(self, X, Y):
+        print("we have go to loss function!")
+        P, sequence_length = self.inference(X)
+        print("We have the inference P!!!")
+        cross_entropy2_step1=tf.nn.softmax_cross_entropy_with_logits(labels=Y,logits=P)
+        loss=tf.reduce_sum(cross_entropy2_step1)
+        return P, loss
+    
+    def load_w2v(self, dic_path):
+        print("Start loading dict")
+        with open(dic_path, "r") as f:
+            word_dic = json.load(f)
+        print("End loading dict")
+        return word_dic
+    
+    def create_file_vector(self, file_origin_list, embedding_size):
+        word_dic = self.c2v
+        #Create word vector array
+        word_vector = []
+        for article in file_origin_list:
+            article_vector = []
+            for word in article:
+                if word_dic.has_key(word):
+                    article_vector.append(np.array(word_dic[word]))
+                else:
+                    article_vector.append([0.0] * embedding_size)
+            if (len(article_vector) < 80):
+                for comp_index in xrange(0, (80 - len(article_vector))):
+                    article_vector.append([0.0] * embedding_size)
+            word_vector.append(np.array(article_vector))
+        return np.array(word_vector)
+
+    def test_unary_score(self):
+        P, sequence_length = self.inference(self.inp,
+                                            reuse=True,
+                                            trainMode=False)
+        return P, sequence_length
+
+
+def read_csv(batch_size, file_name):
+    filename_queue = tf.train.string_input_producer([file_name])
+    reader = tf.TextLineReader(skip_header_lines=0)
+    key, value = reader.read(filename_queue)
+    # decode_csv will convert a Tensor from type string (the text line) in
+    # a tuple of tensor columns with the specified defaults, which also
+    # sets the data type for each column
+    decoded = tf.decode_csv(
+        value,
+        field_delim=' ',
+        record_defaults=[[0] for i in range(FLAGS.max_sentence_len * 2)])
+
+    # batch actually reads the file and loads "batch_size" rows in a single
+    # tensor
+    return tf.train.shuffle_batch(decoded,
+                                  batch_size=batch_size,
+                                  capacity=batch_size * 50,
+                                  min_after_dequeue=batch_size)
+
+
+def test_evaluate(sess, unary_score, test_sequence_length, inp,
+                  tX, tY):
+    totalEqual = 0
+    batchSize = FLAGS.batch_size
+    totalLen = tX.shape[0]
+    numBatch = int((tX.shape[0] - 1) / batchSize) + 1
+    correct_labels = 0
+    total_labels = 0
+    
+    pred_labels = []
+    for i in range(numBatch):
+        endOff = (i + 1) * batchSize
+        if endOff > totalLen:
+            endOff = totalLen
+        feed_dict = {inp: tX[i * batchSize:endOff]}
+        unary_score_val, test_sequence_length_val = sess.run(
+            [unary_score, test_sequence_length], feed_dict)
+        label = []
+        for i in unary_score_val:
+            for j in i:
+                max_val = np.argmax(j)
+                label.append(max_val)
+        pred_labels.extend(label)
+                
+        
+        
+    new_Y = tY.reshape(-1,)
+    result = accuracy_score(pred_labels, new_Y)
+
+    print("The accuracy score!!!")
+    print(result)
+    return result
+def train(total_loss):
+    return tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(total_loss)
             
 def main():
-    train_word_file = "/Users/flynn/Desktop/DaGuan/cnn_classification/train_data/sep_word.txt"
-    train_label_file = "/Users/flynn/Desktop/DaGuan/cnn_classification/train_data/sep_label.txt"
-    dict_file = "/Users/flynn/Desktop/DaGuan/new_data/word_dic_64.json"
-    #model_save_path = "/home/chenyu/daguan/model/basic_svm"
+    trainDataPath = '/Users/flynn/Desktop/DaGuan/daguan/trainMatrix.txt'
+    graph = tf.Graph()
 
-    file_vector = create_file_vector(train_word_file, dict_file, embedding_size = 64)
+    load_Y = np.loadtxt("sep_label.txt")
+    Y_single = []
+    for i in load_Y:
+        Y_single.append([int(i)])
+    load_Y = np.array(Y_single)
 
-    with open(train_label_file, 'r') as f:
-        label = []
-        line = f.readline()
-        while line:
-            line = line.strip()
-            label.append(int(line))
-            line = f.readline()
+    X_matrix = np.loadtxt(trainDataPath)
+    X_matrix = X_matrix.astype(int)
 
-    label = np.array(label)
-    label = label - 1
-    Y_label = np_utils.to_categorical(label, 14)
-    file_vector = file_vector.reshape(file_vector.shape[0], 100, 64,1)
-    file_vector = file_vector.astype('float32')
+    with graph.as_default():
+        model = Model(FLAGS.embedding_size, FLAGS.num_tags,
+                      FLAGS.word2vec_path, FLAGS.num_hidden)
+        print("train data path:", trainDataPath)
+        encoded_Y = tf.one_hot(load_Y, 14,
+               on_value=1.0, off_value=0.0,
+               axis=-1)
+        P, total_loss = model.loss(X_matrix, encoded_Y)
+        train_op = train(total_loss)
+        test_unary_score, test_sequence_length = model.test_unary_score()
+    #     sv = tf.train.Supervisor(graph=graph, logdir=FLAGS.log_dir)
+        sv = tf.train.Supervisor(graph=graph, logdir=None)
+        with sv.managed_session(master='') as sess:
+            print("This is the Y label")
+            print("the Y-matrix")
+            # actual training loop
+            training_steps = FLAGS.train_steps
+            trackHist = 0
+            bestAcc = 0
 
-    model = Sequential()
-    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(100,64,1)))
-    model.add(Convolution2D(32, 3, 3, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(14, activation='softmax'))
-
-    model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-    model.fit(file_vector, Y_label, 
-          batch_size=10, nb_epoch=10, verbose=1)
-
-    # basic_model = BaselineModel(64)
-    
-    # train_vector, train_label, train_cv_vector, train_cv_label = generate_model_input_data(train_word_file, train_label_file, dict_file)
-    
-    # c_value = [1, 10, 50, 100, 500]
-    # for c in c_value:
-    #     model_save_path = "/Users/flynn/Desktop/DaGuan/daguan/model/basic_svm_" + str(c)
-    #     basic_model.fit(c, model_save_path, train_vector, train_label)
-    #     train_predict_result = basic_model.predict(model_save_path, train_vector)
-    #     cv_predict_result = basic_model.predict(model_save_path, train_cv_vector)
-        
-    #     accuracy_score = metrics.precision_score(train_label, train_predict_result, average='micro') 
-    #     F1_score = metrics.f1_score(train_label, train_predict_result, average='weighted')  
-    #     log.info("The following is the result for c value " + str(c))
-    #     log.info("The accuracy for train data is " + str(accuracy_score))
-    #     log.info("The f1 score for train data is " + str(F1_score))
-        
-    #     accuracy_score = metrics.precision_score(train_cv_label, cv_predict_result, average='micro') 
-    #     F1_score = metrics.f1_score(train_cv_label, cv_predict_result, average='weighted')  
-    #     log.info("The accuracy for cv data is " + str(accuracy_score))
-    #     log.info("The f1 score for cv data is " + str(F1_score))
+            print("NOW WE AER IN THE STEPS!")
+            for step in range(training_steps):
+                if sv.should_stop():
+                    break
+                try:
+                    sess.run(train_op)
+                    if (step + 1) % 10 == 0:
+                        print("[%d] loss: [%r]" %
+                              (step + 1, sess.run(total_loss)))
+                        X_test = model.create_file_vector(X_matrix, embedding_size = 64)
+                        
+                        acc = test_evaluate(sess, test_unary_score,
+                                            test_sequence_length,
+                                            model.inp, X_test, load_Y)
+                        if acc > bestAcc:
+                            bestAcc = acc
+                            trackHist = 0
+                        elif trackHist > FLAGS.track_history:
+                            print(
+                                "always not good enough in last %d histories, best accuracy:%.3f"
+                                % (trackHist, bestAcc))
+                            break
+                        else:
+                            trackHist += 1
+                except KeyboardInterrupt, e:
+                    raise e
         
 if __name__=='__main__':
     main()
